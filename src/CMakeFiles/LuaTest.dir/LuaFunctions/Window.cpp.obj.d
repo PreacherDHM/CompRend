6709806cf2ee75dd6ef21aca445e2f97da3863cc
PreacherDHM <jesseh2003@outlook.com>
2024-10-14 18:12:09 -0700

    second commit

--- src/CMakeFiles/LuaTest.dir/LuaFunctions/Window.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/LuaTest.dir/LuaFunctions/Window.cpp.obj: \
- C:\Users\jesse\OneDrive\Documents\Projects\LuaTesting\src\LuaFunctions\Window.cpp \
- C:\Users\jesse\OneDrive\Documents\Projects\LuaTesting\src\LuaFunctions\Window.h
